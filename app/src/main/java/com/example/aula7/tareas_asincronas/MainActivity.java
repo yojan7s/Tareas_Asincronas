a5461810a6c18b783b7afe89beacf5488987ad28
pc710 <pc710@iucesmag.edu.co>
2018-04-10 13:50:56 -0500

    final

--- app/src/main/java/com/example/aula7/tareas_asincronas/MainActivity.java
+++ app/src/main/java/com/example/aula7/tareas_asincronas/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.aula7.tareas_asincronas;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,8 +42,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //evento para el boton principal
-    public  void  loadData(){
+    public  void  loadData(View view){
+        if (isOnLine()){
+            MyTask myTask = new MyTask();
+            myTask.execute();
+        }else {
+            Toast.makeText(this, "Si hay conexion", Toast.LENGTH_SHORT).show();
+        }
 
+    }
+
+    //Metodo para procesar los datos
+    public void processData(String s){
+        textView.setText("Numero" +s);
+        textView.setTextSize(Integer.parseInt(s));
+
+    }
+
+    public class MyTask extends AsyncTask<String, String, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for (int i = 1; i<=50; i++){
+                publishProgress(String.valueOf(i));
+            }
+            return "Fin";
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            processData(values[0]);
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
 }
